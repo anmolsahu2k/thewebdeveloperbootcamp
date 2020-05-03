45cd98bd526cf2936bd9b17ab03362feab92991b
Anmol Sahu <anmolsahu2k@gmail.com>
2020-05-03 15:29:40 +0530

    Add jQuery files from the wdb

---
+++ jQuery/effects.js
@@ -0,0 +1,12 @@
+$('button').click(function(){
+	// $('div').fadeOut();	
+	// $('.container2').fadeOut(1000,function(){//this callback function executes only when fading ends
+	// 	console.log("Fade Completed!");
+	// });
+
+	// $('.container').fadeIn(1000,function(){//this callback function executes only when fading ends
+	// 	console.log("Fade Completed!");
+	// });
+	$('div').fadeToggle(500);
+	//slideUp(),slideDown(),slideToggle()
+})
